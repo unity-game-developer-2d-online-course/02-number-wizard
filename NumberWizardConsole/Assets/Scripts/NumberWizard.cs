3dd6824dc2c619dc3c286b16c4e50ec1c00258f2
Mattias Strömberg <m.stromberg@outlook.com>
2020-08-05 11:56:32 +0200

    Print to console with Debug.Log()

---
+++ NumberWizardConsole/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Debug.Log("Welcome to Number Wizard!");
+        Debug.Log("Pick a number, don't tell me what it is...");
+        Debug.Log("The highest number you can pick is 1000.");
+        Debug.Log("The lowest number you can pick is 1.");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
