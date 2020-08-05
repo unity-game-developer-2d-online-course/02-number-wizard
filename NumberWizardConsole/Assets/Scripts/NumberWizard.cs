07c83dc1ee945348ed29d7025cb2fc5745c8b278
Mattias Strömberg <m.stromberg@outlook.com>
2020-08-05 14:05:43 +0200

    Introducing variables

--- NumberWizardConsole/Assets/Scripts/NumberWizard.cs
+++ NumberWizardConsole/Assets/Scripts/NumberWizard.cs
@@ -5,10 +5,13 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
+        const int min = 1;
+        const int max = 1000;
+
         Debug.Log("Welcome to Number Wizard!");
         Debug.Log("Pick a number, don't tell me what it is...");
-        Debug.Log("The highest number you can pick is 1000.");
-        Debug.Log("The lowest number you can pick is 1.");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
     }
 
     // Update is called once per frame
