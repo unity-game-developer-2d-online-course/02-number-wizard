4d46a3528f0125077b380763ae3cb8f894d073af
Mattias Strömberg <m.stromberg@outlook.com>
2020-08-07 16:42:21 +0200

    Calculate guess variable

--- NumberWizardConsole/Assets/Scripts/NumberWizard.cs
+++ NumberWizardConsole/Assets/Scripts/NumberWizard.cs
@@ -15,8 +15,10 @@ namespace NumberWizard
             Debug.Log("Pick a number, don't tell me what it is...");
             Debug.Log("The highest number you can pick is: " + max);
             Debug.Log("The lowest number you can pick is: " + min);
-            Debug.Log("Tell me if your number is higher or lower than 500");
+            Debug.Log("Tell me if your number is higher or lower than " + guess);
             Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+
+            max++;
         }
 
         // Update is called once per frame
@@ -24,19 +26,19 @@ namespace NumberWizard
         {
             if (Input.GetKeyDown(KeyCode.Return))
             {
-                Debug.Log("Enter key was pressed");
+                Debug.Log("I am a genius!");
             }
             else if (Input.GetKeyDown(KeyCode.UpArrow))
             {
-                Debug.Log("Up Arrow key was pressed");
                 min = guess;
-                Debug.Log(guess);
+                guess = (max + min) / 2;
+                Debug.Log("Is it higher or lower than " + guess);
             }
             else if (Input.GetKeyDown(KeyCode.DownArrow))
             {
-                Debug.Log("Down Arrow key was pressed");
                 max = guess;
-                Debug.Log(guess);
+                guess = (max + min) / 2;
+                Debug.Log("Is it higher or lower than " + guess);
             }
         }
     }
